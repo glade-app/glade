a7582b5f664fd7b9533dbf2c6395697bcc80c440
allengu01 <allen.gu01@gmail.com>
2020-11-27 00:08:42 -0600

    Temporary solution added for making the loading time faster

--- Glade/View Controllers/StartViewController.swift
+++ Glade/View Controllers/StartViewController.swift
@@ -12,6 +12,8 @@ class StartViewController: UIViewController {
     @IBOutlet weak var verticalStack: UIStackView!
     @IBOutlet weak var gladeNameLabel: UILabel!
     
+    var accessToken: String?
+    
     override func viewDidAppear(_ animated: Bool) {
         let signedUp = UserDefaults.standard.bool(forKey: "signedUp")
 //        let signedUp = false // Testing Purposes
@@ -23,38 +25,10 @@ class StartViewController: UIViewController {
         if signedUp {
             Token.refreshAccessToken() { (result) in
                 let username = UserDefaults.standard.string(forKey: "username")
-                let accessToken = try! Token.getToken("accessToken", username: username!)
-                Spotify.getUserData(accessToken: accessToken) { (result, user) in
+                self.accessToken = try! Token.getToken("accessToken", username: username!)
+                Spotify.getUserData(accessToken: self.accessToken!) { (result, user) in
                     DataStorage.storeUserData(user: user) { result in
-                        // Store artists and songs data to Firestore
-                        let group = DispatchGroup()
-                        let queue = DispatchQueue(label: Bundle.main.bundleIdentifier! + ".datastorage.queue", attributes: .concurrent)
-                        // Request user's top artists from Spotify and save to Firebase
-                        group.enter()
-                        queue.async {
-                            Spotify.getUserTopArtists(accessToken: accessToken) { (result, artists) in
-                                DataStorage.storeUserTopArtists(artists: artists) { (result) in
-                                    print("Finished storing top artists")
-                                    group.leave()
-                                }
-                            }
-                        }
-                
-                        // Request user's top songs from Spotify and save to Firebase
-                        group.enter()
-                        queue.async(group: group) {
-                            Spotify.getUserTopSongs(accessToken: accessToken) { (result, songs) in
-                                DataStorage.storeUserTopSongs(songs: songs) { (result) in
-                                    print("Finished storing top songs")
-                                    group.leave()
-                                }
-                            }
-                        }
-                
-                        group.notify(queue: .main) {
-                            print("Finished storing data to Firestore")
-                            self.performSegue(withIdentifier: "startToMain", sender: self)
-                        }
+                        self.performSegue(withIdentifier: "startToMain", sender: self)
                     }
                 }
             }
@@ -62,6 +36,47 @@ class StartViewController: UIViewController {
         // If user isn't signed up: Send them to the sign up sequence
         else {
             performSegue(withIdentifier: "toSchools", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "startToMain" {
+            let tabBar = segue.destination as! UITabBarController
+            let navBar = tabBar.viewControllers![0] as! UINavigationController
+            let homeVC = navBar.topViewController as! HomeViewController
+            // Store artists and songs data to Firestore
+            let group = DispatchGroup()
+            let queue = DispatchQueue(label: Bundle.main.bundleIdentifier! + ".datastorage.queue", attributes: .concurrent)
+            // Request user's top artists from Spotify and save to Firebase
+            group.enter()
+            queue.async {
+                Spotify.getUserTopArtists(accessToken: self.accessToken!) { (result, artists) in
+                    DataStorage.storeUserTopArtists(artists: artists) { (result) in
+                        print("Finished storing top artists")
+                        group.leave()
+                    }
+                }
+            }
+    
+            // Request user's top songs from Spotify and save to Firebase
+            group.enter()
+            queue.async(group: group) {
+                Spotify.getUserTopSongs(accessToken: self.accessToken!) { (result, songs) in
+                    DataStorage.storeUserTopSongs(songs: songs) { (result) in
+                        print("Finished storing top songs")
+                        group.leave()
+                    }
+                }
+            }
+    
+            group.notify(queue: .main) {
+                print("Finished storing data to Firestore")
+                homeVC.getCurrentUser() { (result) in
+                    homeVC.getArtistsData()
+                    homeVC.getSongsData()
+                }
+            }
+    
         }
     }
     
